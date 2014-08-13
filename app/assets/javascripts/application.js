bb32511575ff3776bad785211a3571afb4937fcb
Sean Smith <seansmith1020@gmail.com>
2014-08-13 07:57:34 -0600

    background color of task changes depending on date

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,37 +10,50 @@ $(function() {
 
 $(document).ready(function(){
 
-  $('.flash').delay(500).hide(400);
+  $('.flash').delay(5000).hide(400);
 
   $('.flash').click(function(){
     $(this).hide();
   });
 
-  var $tasks = $('.task').hide();
-  var $deletes = $('.delete-button').hide();
-  var $completes = $('.complete-button').hide();
+  $('.task').hide();
+  $('.delete-button').hide();
+  $('.complete-button').hide();
 
   $('.expand-list').click(function(){
+    var $clicked = $(this)
     var id = $(this).attr('data-task-id');
 
     $('.task').each(function(){
-      if ($(this).attr('data-task-id') != id){
-        $(this).parents('.this-task-list').find('.delete-button').hide();
-        $(this).parents('.this-task-list').find('.complete-button').hide();
-        $(this).parents('.this-task-list').find('.expand-list').attr('src', 'http://www.clker.com/cliparts/9/e/2/2/13522327172045924093Right%20Arrow%20Button.svg.med.png')
-        $(this).parents('.this-task-list').find('.task').hide();
+      var $this = $(this)
+      if ($this.attr('data-task-id') != id){
+        $this.parents('.this-task-list').find('.delete-button').hide();
+        $this.parents('.this-task-list').find('.complete-button').hide();
+        $this.parents('.this-task-list').find('.expand-list').attr('src', 'http://www.clker.com/cliparts/9/e/2/2/13522327172045924093Right%20Arrow%20Button.svg.med.png')
+        $this.parents('.this-task-list').find('.task').hide();
       }
     });
 
-    if ($(this).attr('src') == 'http://www.clker.com/cliparts/2/9/c/f/13522312291232734589Down%20Arrow%20Button.svg.med.png'){
-      $(this).attr('src', 'http://www.clker.com/cliparts/9/e/2/2/13522327172045924093Right%20Arrow%20Button.svg.med.png')
+    if ($clicked.attr('src') == 'http://www.clker.com/cliparts/2/9/c/f/13522312291232734589Down%20Arrow%20Button.svg.med.png'){
+      $clicked.attr('src', 'http://www.clker.com/cliparts/9/e/2/2/13522327172045924093Right%20Arrow%20Button.svg.med.png')
     }
     else {
-      $(this).attr('src', 'http://www.clker.com/cliparts/2/9/c/f/13522312291232734589Down%20Arrow%20Button.svg.med.png')
+      $clicked.attr('src', 'http://www.clker.com/cliparts/2/9/c/f/13522312291232734589Down%20Arrow%20Button.svg.med.png')
     }
 
-    $(this).parents('.this-task-list').find('.task').slideToggle();
-    $(this).parents('.this-task-list').find('.delete-button').slideToggle();
-    $(this).parents('.this-task-list').find('.complete-button').slideToggle();
+    $clicked.parents('.this-task-list').find('.task').slideToggle();
+    $clicked.parents('.this-task-list').find('.delete-button').slideToggle();
+    $clicked.parents('.this-task-list').find('.complete-button').slideToggle();
   });
+
+  $('.task').each(function(){
+    var $this = $(this)
+    if ($this.attr('data-due-date') == 0 ){
+      $this.parents('.this-task-list').find('.task').css('background-color', 'orange');
+    }
+    else if ($this.attr('data-due-date') < 0 ){
+      $this.parents('.this-task-list').find('.task').css('background-color', 'lightcoral');
+    }
+  });
+
 });
